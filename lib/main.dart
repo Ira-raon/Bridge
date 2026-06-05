ce0cabf5a69a4160e3b5110485898ee9d4f80043
Sakiinah Adeyemi <up2248776@myport.ac.uk>
2026-06-05 16:23:57 +0100

    anonkey and creating database begining

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app.dart';
 
 export 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://mgeilydszmdggyaqbtuu.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nZWlseWRzem1kZ2d5YXFidHV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2NjQzNTcsImV4cCI6MjA5NjI0MDM1N30.RG63pJQv6K4zJk8VErGaXiLQj9GNUDeyqiHMebSDGFM',
+  );
+
   runApp(const BridgeApp());
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,9 +3,25 @@ class AppUser {
   final String email;
   final String? displayName;
 
-  AppUser({
+  const AppUser({
     required this.id,
     required this.email,
     this.displayName,
   });
+
+  factory AppUser.fromMap(Map<String, dynamic> map) {
+    return AppUser(
+      id: map['id'] as String,
+      email: map['email'] as String,
+      displayName: map['display_name'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'email': email,
+      'display_name': displayName,
+    };
+  }
 }
